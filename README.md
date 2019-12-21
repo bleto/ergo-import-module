97dcad4ead56afd296162cdd97f182c88ae54282
Piotr Bletek <p.bletek@gmail.com>
2019-12-21 14:59:05 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+
+<p align="center">
+  <a href="https://vuejs.org" rel="noopener noreferrer">
+    <img width="300" src="https://ergonode.com/wp-content/themes/ergonode/assets/img/logo.svg" alt="Egronode logo">
+  </a>
+</p>
+<p align="center">Ergonode import module</p>
+<p align="center">
+  <a href="https://github.com/softbeat/erg-import-module">
+    <img alt="npm" src="https://img.shields.io/npm/v/@softbeat/erg-import-module">
+  </a>
+  <a href="https://github.com/softbeat/erg-import-module">
+    <img alt="NPM" src="https://img.shields.io/npm/l/@softbeat/erg-import-module">
+  </a>
+</p>
+<br>
---
+++ index.js
@@ -0,0 +1,43 @@
+/*
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE for license details.
+ */
+import { Pages, Icons } from './config/imports';
+import Privileges from './config/privileges';
+
+export default {
+    name: '@ergo/import',
+    type: 'page',
+    moduleRelations: [
+        '@ergo/core',
+    ],
+    privileges: Privileges,
+    nuxt: {
+        aliases: {
+            '@Import': '/',
+        },
+    },
+    router: [
+        {
+            name: 'imports',
+            path: '/imports',
+            component: Pages.Import,
+            meta: {
+                access: true,
+                title: 'Imports',
+                group: {
+                    title: 'Sources',
+                    icon: Icons.Export,
+                },
+                isMenu: true,
+                privileges: Privileges.IMPORT,
+            },
+        },
+    ],
+    store: [
+        {
+            directory: 'import',
+            name: 'import',
+        },
+    ],
+};
